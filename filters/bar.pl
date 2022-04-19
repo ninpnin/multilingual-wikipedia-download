50f18f8baa9935b8c3cc8d787409c58cb75db7f0
ninpnin <vainoyrjanainen@icloud.com>
2022-04-19 22:10:02 +0200

    feat: copy german filter for bavarian

---
+++ filters/bar.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+# Program to filter Wikipedia XML dumps to "clean" text consisting only of lowercase
+# letters (a-z, converted from A-Z), and spaces (never consecutive).
+# All other characters are converted to spaces.  Only text which normally appears
+# in the web browser is displayed.  Tables are removed.  Image captions are
+# preserved.  Links are converted to normal text.  Digits are spelled out.
+
+# Written by Matt Mahoney, June 10, 2006.  This program is released to the public domain.
+
+$/=">";                     # input record separator
+while (<>) {
+  if (/<text /) {$text=1;}  # remove all but between <text> ... </text>
+  if (/#redirect/i) {$text=0;}  # remove #REDIRECT
+  #print "$text \n";
+  if ($text) {    
+    # Remove any text not normally visible
+    if (/<\/text>/) {$text=0;}
+    s/<.*>//;               # remove xml tags
+    
+    s/&amp;/&/g;            # decode URL encoded chars
+    s/&lt;/</g;
+    s/&gt;/>/g;
+    s/<ref[^<]*<\/ref>//g;  # remove references <ref...> ... </ref>
+    s/<[^>]*>//g;           # remove xhtml tags
+    s/\[http:[^] ]*/[/g;    # remove normal url, preserve visible text
+    s/\|thumb//ig;          # remove images links, preserve caption
+    s/\|left//ig;
+    s/\|right//ig;
+    s/\|\d+px//ig;
+    s/\[\[image:[^\[\]]*\|//ig;
+    s/\[\[category:([^|\]]*)[^]]*\]\]/[[$1]]/ig;  # show categories without markup
+    s/\[\[[a-z\-]*:[^\]]*\]\]//g;  # remove links to other languages
+    s/\[\[[^\|\]]*\|/[[/g;  # remove wiki url, preserve visible text
+    s/\{\{[^\}]*\}\}//g;         # remove {{icons}} and {tables}
+    s/\{[^\}]*\}//g;
+    s/\[//g;                # remove [ and ]
+    s/\]//g;
+    s/&[^;]*;/ /g;          # remove URL encoded chars
+    
+    # convert to lowercase letters and spaces, spell digits
+    $_=" $_ ";
+    tr/A-Z/a-z/;
+    s/Ä/ä/g;
+    s/Ö/ö/g;
+    s/Ü/ö/g;
+    s/0/ null /g;
+    s/1/ eins /g;
+    s/2/ zwei /g;
+    s/3/ drei /g;
+    s/4/ vier /g;
+    s/5/ fünf /g;
+    s/6/ sechs /g;
+    s/7/ sieben /g;
+    s/8/ acht /g;
+    s/9/ neun /g;
+    tr/a-zäöüß\n/ /cs;
+    chop;
+    print $_;
+  }
+}
